483b73826983306916841306abf9c597afbc08d7
Radu Alexandra <radualexandra268@yahoo.co>
2019-12-02 22:16:13 +0200

    First version of the app

---
+++ Bullseye/AboutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutViewController.swift
+//  Bullseye
+//
+//  Created by Madalina on 02/12/2019.
+//  Copyright © 2019 Alexandra Radu. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+    @IBAction func close (){
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    
+}
---
+++ Bullseye/AuthorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AuthorViewController.swift
+//  Bullseye
+//
+//  Created by Madalina on 02/12/2019.
+//  Copyright © 2019 Alexandra Radu. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close (){
+          dismiss(animated: true, completion: nil)
+      }
+      
+  
+
+}
